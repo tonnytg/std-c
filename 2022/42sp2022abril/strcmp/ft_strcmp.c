5b9e26c5757bfb0c7d14166613b952cc63817e6c
Antonio Thomacelli <tonnytg@gmail.com>
2022-04-08 08:13:31 -0300

    feat: reducing lines of code

--- 2022/42sp2022abril/strcmp/ft_strcmp.c
+++ 2022/42sp2022abril/strcmp/ft_strcmp.c
@@ -9,17 +9,12 @@ int ft_strcmp(char *s1, char *s2) {
         }
         i++;
     } while (s1[i] != '\0' || s2[i] != '\0');
-
-    if ((s1[i] == '\0' && s2[i] != '\0') || (s2[i] == '\0' && s1[i] != '\0')) {
-        return (s1[i] - s2[i]);
-    } else {
-        return (0);
-    }
+    return (0);
 }
 
 int main(void)
 {
-    char name[] = "Antonio";
+    char name[] = "";
     char last[] = "Antonio";
 
     printf("difference: %d\n", ft_strcmp(name, last));
