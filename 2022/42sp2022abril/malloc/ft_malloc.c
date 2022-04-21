4cabcab72b9b4a4567968698d6cd984c13b05f76
Antonio Thomacelli <tonnytg@gmail.com>
2022-04-20 21:09:46 -0300

    feat: add recursion fibonacci

---
+++ 2022/42sp2022abril/malloc/ft_malloc.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+        i++;
+    return (i);
+}
+
+char *ft_strcpy(char *dest, char *src)
+{
+    int i;
+
+    i = 0;
+    while (src[i] !=  '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    return (dest);
+}
+
+char *ft_malloc(char *str)
+{
+    int length;
+    char *tmp;
+
+    length = ft_strlen(str);
+    tmp = (char *)malloc(length + 1);
+    ft_strcpy(tmp, str);
+    return (tmp);
+}
+
+int main()
+{
+    char name[15] = "Antonio";
+    char *copy;
+
+    copy = ft_malloc(name);
+    printf("before free:%s\n", copy);
+    free(copy);
+    printf("after free:%s\n", copy);
+}
